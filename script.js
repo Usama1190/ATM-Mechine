ec7cb570f1c9eb7f92c13addd6229ce92fee500d
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-03-28 02:01:24 -0700

    ATM Mechine on browser

---
+++ script.js
@@ -0,0 +1,27 @@
+// front screen hide
+$(document).ready(function() {
+    $('#front-screen').click(function() {
+        $('#front-screen').hide();
+    });
+});
+
+// second screen hide
+$(document).ready(function() {
+    $('#btn-1').click(function() {
+        $('#second-screen').hide();
+    });
+});
+
+// third screen hide
+$(document).ready(function() {
+    $('#btn-2').click(function() {
+        $('#third-screen').hide();
+    });
+});
+
+// fourth screen hide
+$(document).ready(function() {
+    $('#btn-3').click(function() {
+        $('#fourth-screen').hide();
+    });
+});
