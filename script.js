129561d33f2d750b4fdd5585578e885066f3d3a7
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-03-29 08:25:55 -0700

    Update ATM

--- script.js
+++ script.js
@@ -37,13 +37,14 @@ function checkPin() {
     }
     else {
         document.getElementById('para-1').innerHTML = 'Invalid pin code';
-        document.getElementById('para-1').style.color = 'red';
+        document.getElementById('para-2').innerHTML = 'hint: 12345';
+        document.getElementById('para-1').style.color = '#bb2124';
     }
 }
 
 
 function amountWithDraw() {
-    // valid_amount = 10000;
+    // var valid_amount = 10000;
 
     var user_input_amount = document.getElementById('input-2').value;
 
@@ -52,7 +53,7 @@ function amountWithDraw() {
 
         remaining_amount = valid_amount - user_input_amount;
         document.getElementById('info-1').innerHTML = 'Your previous amount is: $' + valid_amount + '/-';
-        document.getElementById('info-2').innerHTML = 'Your widthdraw is: $' + user_input_amount + '/-';
+        document.getElementById('info-2').innerHTML = 'Your withdraw is: $' + user_input_amount + '/-';
         document.getElementById('info-3').innerHTML = 'Your remaining amount is: $' + remaining_amount + '/-';
     }
     else {
